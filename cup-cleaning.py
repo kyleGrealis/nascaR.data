bd8b9801c336fdae7c7f6dbfb42f37a4bf4ba3cf
Kyle Grealis <84798289+kyleGrealis@users.noreply.github.com>
2024-06-04 10:57:03 -0500

    initial commit

---
+++ cup-cleaning.py
@@ -0,0 +1,3 @@
+'''
+This is the data cleaning script for NASCAR Cup series data. Some level of data exploration is necessary, but the primary purpose is to clean the data and create new variables.
+'''
