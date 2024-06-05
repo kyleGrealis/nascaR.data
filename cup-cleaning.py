18a63ec3b4da4a2ab6458e734d7e73300965c602
Kyle Grealis <84798289+kyleGrealis@users.noreply.github.com>
2024-06-04 19:02:29 -0500

    edit: separated sponsor and owner

--- cup-cleaning.py
+++ cup-cleaning.py
@@ -1,3 +1,43 @@
 '''
 This is the data cleaning script for NASCAR Cup series data. Some level of data exploration is necessary, but the primary purpose is to clean the data and create new variables.
 '''
+
+# %%
+import numpy as np
+import pandas as pd
+import polars as pl
+import re
+
+
+# %%
+cup = (
+  pl.read_csv('data/cup-series/all-cup-series-results.csv', ignore_errors=True)
+  .rename({
+    'Pos': 'Finish',
+    'St': 'Start',
+    '#': 'Car_number',
+    'Car': 'Car_model'
+  })
+  .with_columns(
+    pl.col('Sponsor / Owner').fill_null('').alias('Sponsor / Owner'),
+    pl.col('Car_model').fill_null('').alias('Car_model'),
+    pl.col('Status').fill_null('').alias('Status')
+  )
+  .with_columns(
+      # create 'Sponsor' column by removing content within parentheses and strip whitespace, or set to empty string if no parentheses
+      pl.col('Sponsor / Owner')
+      .map_elements(lambda value: re.sub(r'\(.*?\)', '', value).strip() if re.search(r'\(.*?\)', value) else "", return_dtype=pl.Utf8) 
+      .alias('Sponsor')
+    )
+    .with_columns(
+      # create 'Owner' column by extracting content within parentheses or using the entire value if no parentheses
+      pl.col('Sponsor / Owner')
+      .map_elements(lambda value: re.search(r'\((.*?)\)', value).group(1) if re.search(r'\((.*?)\)', value) else value, return_dtype=pl.Utf8)
+      .alias('Owner')
+    )
+)
+
+
+
+
+# %%
