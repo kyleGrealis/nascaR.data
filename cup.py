771eccae049a2cc295ee0e0f6c43bb2eb61b6163
Kyle Grealis <84798289+kyleGrealis@users.noreply.github.com>
2024-06-12 14:45:38 -0500

    edit: adding track details

--- cup.py
+++ cup.py
@@ -6,11 +6,12 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import os
+import re
 import time
 from io import StringIO
 
 # function to get the race results informaton
-def race_results(driver, race_link, season, race_num, track):
+def race_results(driver, race_link, season, race_num, site):
   
     # get the HTML of the page and parse with BeautifulSoup
     soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -29,33 +30,54 @@ def race_results(driver, race_link, season, race_num, track):
     # save the original column names
     original_columns = race_table.columns.tolist()
 
+    # find the track name; not always the same as the location/city
+    track = next((p.find('a', href=lambda h: "tracks" in h).text for p in soup.find_all('p') if p.text and "race number" in p.text), None)
+
+    # find the track length and convert it to a float
+    track_length = next((float(re.search(r'(\d+\.?\d*) mile', p.text).group(1)) for p in soup.find_all('p') if p.text and re.search(r'(\d+\.?\d*) mile', p.text)), None)
+
+    # find the track type (dirt track, road course, speedway, etc)
+    track_type = next((p.text.split('mile')[1].split('(')[0].strip() for p in soup.find_all('p') if p.text and 'mile' in p.text and '(' in p.text), None)
+
     # add season, race number, and site to the DataFrame
     race_table['Season'] = season
     race_table['Race'] = race_num
+    race_table['Site'] = site
     race_table['Track'] = track
+    race_table['Track_length'] = track_length
+    race_table['Track_type'] = track_type
 
     # reorder the columns
-    race_table = race_table[['Season', 'Race', 'Track'] + original_columns]
+    race_table = race_table[
+       ['Season', 'Race', 'Site', 'Track', 'Track_length', 'Track_type'] 
+       + original_columns
+    ]
 
     return race_table
 
 # function to get all season race results from start to stop year
 def cup_racing(start, stop):
     
+    # base url for the racing results
     base_url = 'https://www.racing-reference.info/season-stats/{}/W/'
     
+    # create a list of the seasons
     seasons = list(np.arange(start, stop+1))
     
+    # list to store all of the race results tables by season
     all_race_tables = []
     
     # open firefox
     driver = webdriver.Firefox()
-    # wait max 5 minutes to load page
+    # set the maximum page load timeout
     driver.set_page_load_timeout(600)
 
+    # loop over each season
     for season in seasons:
+        # list to store all of the race results tables by race
         season_race_tables = []
         
+        # load the season page that contains hyperlinks to the results for each race
         driver.get(base_url.format(season))
         
         # get the HTML of the page and parse with BeautifulSoup
@@ -65,16 +87,16 @@ def cup_racing(start, stop):
         divs = soup.find_all('div', class_='race-number')
         race_links = [a['href'] for div in divs for a in div.find_all('a')]
         
-        # get the race track name
-        track_divs = soup.find_all('div', class_='track')
-        tracks = [a.text for div in track_divs for a in div.find_all('a')]
+        # get the race track locaton from the "Site" column in the main season table
+        site_divs = soup.find_all('div', class_='track')
+        sites = [a.text for div in site_divs for a in div.find_all('a')]
         
         # get race info for each race
         for i, race_link in enumerate(race_links):
           for attempt in range(5):
             try:
               driver.get(race_link)
-              race_table = race_results(driver, race_link, season, i+1, tracks[i])
+              race_table = race_results(driver, race_link, season, i+1, sites[i])
               season_race_tables.append(race_table)
               break
             except Exception as e:
@@ -134,10 +156,3 @@ end_time = time.time()
 
 print(f'The process took {end_time-start_time} seconds.')
 
-
-
-# %%
-# import polars as pl
-# cup_pl = pl.from_pandas(cup)
-# cup_pl.head() 
-# cup_pl.columns
