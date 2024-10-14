e472eddce67086626596d3dfa61b82841ed5746c
Kyle Grealis <kyleGrealis@icloud.com>
2024-10-14 15:49:14 -0500

    initial commit; matches get_driver_info for manufacturer

--- get_manufacturer_info.R
+++ get_manufacturer_info.R
@@ -1,37 +1,84 @@
 library(tidyverse)
 
-# First write a function to fuzzy find a manufacturer.
-# Example: "Jimmy Johnson" or "Jimmi Jahnsen" would get "Jimmie Johnson"
-find_similar_manufacturer <- function(name) {
-  name <- str_to_lower(name)
-  manufacturer_list <- cup_series |> 
-    mutate(manufacturer = str_to_lower(manufacturer)) |> 
-    pull(manufacturer)
-  distances <- stringdist::stringdist(name, manufacturer_list, method = 'lv')
-  closest_match <- manufacturer_list[which.min(distances)]
-  return(str_to_title(closest_match))
+# Filter all data into the user's selected race series: Cup, Xfinity, Truck, or all
+selected_series_data <- function(series) {
+  the_series <- str_to_lower(series)
+  all_race_results <- rbind(cup_series, xfinity_series, truck_series)
+  if (series == 'all') {
+    return(all_race_results)
+  } else if (the_series %in% c('cup', 'xfinity', 'truck')) {
+    filtered <- all_race_results |> 
+      mutate(series = str_to_lower(series)) |>
+      filter(series == the_series)
+    return(filtered)
+  } else {
+    stop(paste(str_to_title(series), 'series does not exist.'))
+  }
 }
 
-# Aggregate manufacturer information
-# type: career, season, summary
-filter_manufacturer_info <- function(the_manufacturer, type) {
+# Fuzzy find a manufacturer.
+find_manufacturer <- function(df, manufacturer) {
+  if (manufacturer %in% c('Chevy', 'chevy')) {
+    manufacturer <- 'chevrolet'
+  }
+  
+  # Create a list of manufacturers
+  manufacturer_list <- df |> 
+    mutate(manufacturer = str_to_lower(manufacturer)) |> 
+    pull(manufacturer)
 
+  # Calculate distance of entered name and those in list of manufacturers
+  entered_name <- str_to_lower(manufacturer)
+  distances <- stringdist::stringdist(entered_name, manufacturer_list, method = 'lv')
+  closest_match <- manufacturer_list[which.min(distances)]
+
+  if (entered_name != str_to_lower(closest_match)) {
+    # Get user input if the entered name does not match available manufacturers
+    entered_name <- str_to_title(entered_name)
+    found_name <- str_to_title(closest_match)
+    answer <- str_to_lower(readline(
+      glue::glue(
+        '\n\nI was unable to find "{entered_name}" but found {found_name}. \nIs this who you meant? [y/n] '
+      )
+    ))
+    if (answer %in% c('y', 'yes', 'ye', 'yeah', 'yup')) {
+      return(closest_match)
+    } else {
+      stop('\nPlease check the spelling & try your search function again.')
+    }
+  }
+  return(closest_match)
+}
+
+
+
+# Filter race data based on selected manufacturer
+filter_data <- function(race_data, the_manufacturer) {
   race_results <- 
-    cup_series |>
-    filter(manufacturer == the_manufacturer)
+    race_data |>
+    filter(manufacturer == str_to_title(the_manufacturer)) |>
+    mutate(win = if_else(finish == 1, 1, 0))
+  return(race_results)  
+}
 
-  if (type == 'career') {
-    manufacturer_table <<- race_results
-    glue::glue(
-      "{the_manufacturer}'s results have been saved to the global environment as `manufacturer_table`."
-    )
-  } else if (type == 'season') {
+
+
+
+get_manufacturer_info <- function(manufacturer, series = 'all', type = 'summary') {
+
+  race_series <- selected_series_data(series = series)
+  the_manufacturer <- find_manufacturer(df = race_series, manufacturer = manufacturer)
+  race_results <- filter_data(race_data = race_series, the_manufacturer = the_manufacturer)
+
+  message(str_to_title(the_manufacturer))
+
+  if (type == 'season') {
     manufacturer_table <- 
       race_results |>
-      group_by(season) |>
+      group_by(series, season) |>
       summarize(
         season_races = n_distinct(race_name),
-        number_of_drivers = n_distinct(driver),
+        wins = sum(win, na.rm = TRUE),
         best_finish = min(finish),
         average_finish = round(mean(finish, na.rm = TRUE), 1),
         laps_raced = sum(laps, na.rm = TRUE),
@@ -42,10 +89,11 @@ filter_manufacturer_info <- function(the_manufacturer, type) {
   } else if (type == 'summary') {
     manufacturer_table <- 
       race_results |>
+      group_by(series) |>
       summarize(
         number_of_seasons = n_distinct(season),
-        number_of_drivers = n_distinct(driver),
-        total_races = nrow(race_results),
+        manufacturer_races = n(),
+        wins = sum(win, na.rm = TRUE),
         best_finish = min(finish),
         average_finish = round(mean(finish, na.rm = TRUE), 1),
         laps_raced = sum(laps, na.rm = TRUE),
@@ -53,35 +101,10 @@ filter_manufacturer_info <- function(the_manufacturer, type) {
         total_money = scales::dollar(sum(money, na.rm = TRUE))
       )
       return(manufacturer_table)
-  }
-  
-  # return(manufacturer_table)
-}
-
-get_manufacturer_info <- function(name, type = 'summary') {
-  name <- str_to_title(name)
-  if (find_similar_manufacturer(name) != name) {
-    # Get user input if the entered name does not match available manufacturers
-    answer <- readline(
-      glue::glue(
-        '\n\nI was unable to find "{name}" but found {find_similar_manufacturer(name)}. \nIs this what you meant? [y/n] '
-      )
-    )
-    if (str_to_lower(answer) %in% c('y', 'yes', 'ye', 'yeah', 'yup')) {
-      name <- find_similar_manufacturer(name)
-      message(name)
-      filter_manufacturer_info(name, type = type)
-    } else {
-      message('\nPlease check the spelling & try your search function again.')
-    }
+  } else if (type == 'all') {  
+    # table of manufacturer across all series
+    return(race_results)
   } else {
-    # this will return the list of the manufacturer information
-    message(name)
-    filter_manufacturer_info(name, type = type)
+    stop(paste('Unknown `type =', type, '` entered. Please try again.'))
   }
 }
-
-# get_manufacturer_info('Toyota', type = 'season')
-# get_manufacturer_info('Toyota', type = 'career')
-# get_manufacturer_info('toyata', type = 'summary')
-# get_manufacturer_info('Chevrolet', type = 'summary')
