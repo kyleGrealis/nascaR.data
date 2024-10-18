010abf5077cbf12913832bd6dd555ab4888d2d86
Kyle Grealis <kyleGrealis@icloud.com>
2024-10-17 20:17:39 -0500

    updating package docs

--- R/nascaR.data-package.R
+++ R/nascaR.data-package.R
@@ -3,7 +3,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom dplyr bind_rows filter mutate pull
+#' @importFrom dplyr bind_rows filter if_else mutate pull
 #' @importFrom glue glue
 #' @importFrom stringdist stringdist
 #' @importFrom stringr str_to_lower str_to_title
