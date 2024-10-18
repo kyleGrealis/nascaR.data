7e1990bc91bd8a647b925824b8af79b69a2f933f
Kyle Grealis <kyleGrealis@icloud.com>
2024-10-17 20:17:28 -0500

    updating helper function docs

--- R/helpers.R
+++ R/helpers.R
@@ -25,14 +25,21 @@ NULL
 
 selected_series_data <- function(series) {
   the_series <- str_to_lower(series)
-  all_race_results <- bind_rows(cup_series, xfinity_series, truck_series)
+  
   if (series == 'all') {
+    all_race_results <- bind_rows(
+      cup_series, 
+      xfinity_series, 
+      truck_series
+    )
     return(all_race_results)
+
   } else if (the_series %in% c('cup', 'xfinity', 'truck')) {
     filtered <- all_race_results |> 
       mutate(series = str_to_lower(series)) |>
       filter(series == the_series)
     return(filtered)
+
   } else {
     stop(paste(str_to_title(series), 'series does not exist.'))
   }
@@ -53,7 +60,7 @@ NULL
 #' @return The closest matching driver name as a string.
 #' @examples
 #' \dontrun{
-#' # Example: "Jimmy Johnson" or "Jimmi Jahnsen" would get "Jimmie Johnson"
+#' # Example: "Jimmy Johnson" or "Jimmi Jahnsen" would find "Jimmie Johnson"
 #' find_driver(driver_data, "Jimmy Johnson")
 #' # I was unable to find "Jimmy Johnson" but found Jimmie Johnson. 
 #' # Is this who you meant? [y/n] 
@@ -72,15 +79,18 @@ find_driver <- function(df, driver) {
   # Find the closest match by distance
   closest_match <- driver_list[which.min(distances)]
 
+  # Get user input if the entered name does not match available drivers
   if (entered_name != str_to_lower(closest_match)) {
-    # Get user input if the entered name does not match available drivers
     entered_name <- str_to_title(entered_name)
     found_name <- str_to_title(closest_match)
+
+    # Feedback prompt:
     answer <- str_to_lower(readline(
       glue(
         '\n\nI was unable to find "{entered_name}" but found {found_name}. \nIs this who you meant? [y/n] '
       )
     ))
+    # User response:
     if (answer %in% c('y', 'yes', 'ye', 'yeah', 'yup')) {
       return(closest_match)
     } else {
@@ -90,3 +100,28 @@ find_driver <- function(df, driver) {
   return(closest_match)
 }
 NULL
+
+
+
+#' Filter race data based on selected driver
+#' 
+#' Internal helper function to filter race data based on the selected driver.
+#' 
+#' @name filter_driver_data
+#' @keywords internal
+#' @param race_data The data frame returned from `selected_series_data()`.
+#' @param the_driver The name of the driver returned from `find_driver()`.
+#' @return A filtered data frame of `class()` "tbl_df" containing race results 
+#' of the selected driver. A win column is added for summary statistics.
+#' @examples
+#' \dontrun{
+#' filter_driver_data(cup_series, 'Jimmie Johnson')
+#' }
+
+filter_driver_data <- function(race_data, the_driver) {
+  race_results <- 
+    race_data |>
+    filter(driver == str_to_title(the_driver)) |>
+    mutate(win = if_else(finish == 1, 1, 0))
+  return(race_results)  
+}
